4b9563009c9ca833342c66735c4a8d9b542631b3
Muhammad Salman Sarwar <muhammadsalmansarwar32@gmail.com>
2023-07-21 13:10:38 +0500

    a

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -3,7 +3,7 @@
 import { Provider } from "react-redux"
 import {store} from './store'
 
-export function Providers({children} : {children : any}) {a
+export function Providers({children} : {children : any}) {
     return (
         <Provider store={store}> 
             {children}
